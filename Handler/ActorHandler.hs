b606c2d48d0587e5f7dc9096f8ed54cf1c1f6b0e
vitalijzad <vitalij_zad@libero.it>
2017-04-04 11:39:31 +0200

    Added comments and modified README

--- Handler/ActorHandler.hs
+++ Handler/ActorHandler.hs
@@ -14,39 +14,54 @@ getActorListR = do
 
 getNewActorR :: Handler Html
 getNewActorR = do
+    -- Just show the actor-new view
     defaultLayout $ do
         $(widgetFile "actor-new")
 
 postCreateActorR :: Handler Html
 postCreateActorR = do
+    -- Get the request parameters
     firstName <- runInputPost $ ireq textField "firstName"
     lastName <- runInputPost $ ireq textField "lastName"
+    -- Get the current time
     now <- liftIO getCurrentTime
+    -- Create an actor
     let actor = Actor firstName lastName now
+    -- Insert the actor in the database. Discard the result
     _ <- ($) runDB $ insertEntity actor
     getActorListR
 
 getEditActorR :: Handler Html
 getEditActorR = do
+    -- Get the actor id from the request parameter id
     actorId <- runInputGet $ ireq intField "id"
+    -- Try to read from the database the actor with that id
     maybeActor <- runDB $ get $ ActorKey actorId
     case maybeActor of
+        -- If the actor is found, show the page to edit it
         Just actor -> defaultLayout $ do
             $(widgetFile "actor-edit")
+        -- Otherwise, just show the actor list
         Nothing -> getActorListR
     
 postUpdateActorR :: Handler Html
 postUpdateActorR = do
+    -- Get the parameters from the request
     actorId <- runInputPost $ ireq intField "actorId"
     firstName <- runInputPost $ ireq textField "firstName"
     lastName <- runInputPost $ ireq textField "lastName"
+    -- Get the current time
     now <- liftIO getCurrentTime
+    -- Create an Actor instance
     let actor = Actor firstName lastName now
+    -- Replace the old actor data with the new values
     runDB $ replace (ActorKey actorId) $ actor
     getActorListR
 
 getDeleteActorR :: Handler Html
 getDeleteActorR = do
+    -- Get the id of the Actor to delete from the request
     actorId <- runInputGet $ ireq intField "id"
+    -- Delete the actor
     runDB $ delete (ActorKey actorId)
     getActorListR
--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 This is an example web application example that uses the [MySQL Sakila sample database](https://dev.mysql.com/doc/sakila/en/) and the Haskell web framework [Yesod](http://www.yesodweb.com/).
 
+It is a classic CRUD application, meaning that it does Create, Update and Delete records from the database.
+
 To run it, you need to have [stack](https://docs.haskellstack.org/en/stable/README/) installed.
+Also, you need to have a running MySQL instance and the Sakila database in it.
+You have to put the usual database settings (hostname, port, database name, user, password) in the `config/settings.yml` file.
 
 You can run it by typing:
 
